41a148f1d6584be2ff69130a8d3177380ead132a
Quan Pham <86336291+QunJin@users.noreply.github.com>
2025-09-09 12:13:54 -0600

    week-2

---
+++ app/week-2/student-info.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <h1>QunJin</h1>
+      <p>
+        <Link href="https://github.com/QunJin/cprg306-assignments" target="_blank">
+          Visit my GitHub Repository
+        </Link>
+      </p>
+    </div>
+  );
+}
