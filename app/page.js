3edbaa798085e6cab7bcf69d264539e9f7509e8d
Quan Pham <86336291+QunJin@users.noreply.github.com>
2025-09-16 13:50:18 -0600

    add link

--- app/page.js
+++ app/page.js
@@ -5,8 +5,19 @@ export default function Page() {
   return (
     <div>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <ul> 
-      <li><Link className="text-purple-500 underline hover:text-purple-300" href="./week-2/">Week 2 assignments</Link></li>
+      <ul>
+      {/* week-2  */}
+      <li>
+        <Link className="text-purple-500 underline hover:text-purple-300" href="./week-2/">
+          Week 2 assignments
+        </Link>
+      </li>
+      {/* week-3 */}
+      <li>
+        <Link className="text-purple-500 underline hover:text-purple-300" href="./week-3/">
+          Week 3 assignments
+        </Link>
+      </li>
     
       </ul>
     </div>
